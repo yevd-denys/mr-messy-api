26808e56c6f698127c910fe78cadc7838f6f0272
dyevdushchenko <dyevdushchenko@oneglobus.com>
2019-06-29 11:41:21 +0300

    little fixes

--- src/main/java/com/mrmessy/messynger/interfaces/DataFetcher.java
+++
@@ -1,7 +0,0 @@
-package com.mrmessy.messynger.interfaces;
-
-import graphql.schema.DataFetchingEnvironment;
-
-public interface DataFetcher<T> {
-    T get(DataFetchingEnvironment dataFetchingEnvironment) throws Exception;
-}
